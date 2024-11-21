3f65085f5a23ca5795c5b9cb915a7b2e56481649
Dave232232 <djcclark2012@gmail.com>
2024-11-21 10:28:46 -0500

    added case match to meet equirements

--- menu.py
+++ menu.py
@@ -1,7 +1,7 @@
 # Menu dictionary
 #imports
 import random
-from datetime import date  #from AI overview in google search.  Also used on line 183
+from datetime import date  #from AI overview in google search.  Also used on line 211
 #code
 menu = {
     "Snacks": {
@@ -82,23 +82,23 @@ while place_order:
         i += 1
     print(menu_items)
     # Get the customer's input
-    menu_category = input("Type menu number: ")
+    menu_selection = input("Type menu number: ")
     # Check if the customer's input is a number
-    if menu_category.isdigit():
+    if menu_selection.isdigit():
         # Check if the customer's input is a valid option
-        if int(menu_category) in menu_items.keys():
+        if int(menu_selection) in menu_items.keys():
             # Save the menu category name to a variable
-            menu_category_name = menu_items[int(menu_category)]
+            menu_selection_name = menu_items[int(menu_selection)]
             # Print out the menu category name they selected
-            print(f"You selected {menu_category_name}")
+            print(f"You selected {menu_selection_name}")
 
-            # Print out the menu options from the menu_category_name
-            print(f"What {menu_category_name} item would you like to order?")
+            # Print out the menu options from the menu_selection_name
+            print(f"What {menu_selection_name} item would you like to order?")
             i = 1
             menu_items = {}
             print("Item # | Item name                | Price")
             print("-------|--------------------------|-------")
-            for key, value in menu[menu_category_name].items():
+            for key, value in menu[menu_selection_name].items():
                 # Check if the menu item is a dictionary to handle differently
                 if type(value) is dict:
                     for key2, value2 in value.items():
@@ -120,54 +120,81 @@ while place_order:
                     }
                     i += 1
             # 2. Ask customer to input menu item number
-            menu_category_sub=input("Type menu number: ")
+            menu_selection=input("Type menu number: ")
             # 3. Check if the customer typed a number
-            if menu_category_sub.isdigit():
+            if menu_selection.isdigit():
             # Convert the menu selection to an integer
-                menu_category_sub=int(menu_category_sub)
+                menu_selection=int(menu_selection)
                 # 4. Check if the menu selection is in the menu items
-                if (menu_category_sub) in menu_items.keys():
+                if (menu_selection) in menu_items.keys():
                     # Store the item name as a variable
-                    menu_category_sub_name = menu_items[menu_category_sub]["Item name"]
+                    menu_selection_sub_name = menu_items[menu_selection]["Item name"]
                     #Store it item price as a variable
-                    menu_category_sub_price=menu_items[menu_category_sub]["Price"]
-                    print(f"You selected {menu_category_sub_name}")
+                    menu_selection_sub_price=menu_items[menu_selection]["Price"]
+                    print(f"You selected {menu_selection_sub_name}")
                     # Ask the customer for the quantity of the menu item
-                    menu_category_sub_quantity=input(f"How many {menu_category_sub_name} do you want? ")
+                    quantity=input(f"How many {menu_selection_sub_name} do you want? ")
                     # Check if the quantity is a number, default to 1 if not
-                    if menu_category_sub_quantity.isdigit():
+                    if quantity.isdigit():
+                        #convert the quantity to an integer
+                        quantity=int(quantity)
                         # Add the item name, price, and quantity to the order list
-                        order_list.append({"Item name":menu_category_sub_name,
-                                          "Price":float(menu_category_sub_price),
-                                          "Quantity":int(menu_category_sub_quantity)})   
+                        order_list.append({"Item name":menu_selection_sub_name,
+                                          "Price":float(menu_selection_sub_price),
+                                          "Quantity":int(quantity)})   
                     else:
                         # Tell the customer that their input isn't valid 
-                        print(f"The quantity of {menu_category_sub_name} was not valid.  As a result, your quantity is assumed to be 1.")
-                        menu_category_sub_quantity=1 #default to 1
+                        print(f"The quantity of {menu_selection_sub_name} was not valid.  As a result, your quantity is assumed to be 1.")
+                        quantity=1 #default to 1
                         # Add the item name, price, and quantity to the order list
-                        order_list.append({"Item name":menu_category_sub_name,
-                                          "Price":float(menu_category_sub_price),
-                                          "Quantity":int(menu_category_sub_quantity)})
-                    print(order_list)          
+                        order_list.append({"Item name":menu_selection_sub_name,
+                                          "Price":float(menu_selection_sub_price),
+                                          "Quantity":int(quantity)})   
                 # Tell the customer they didn't select a menu option
             else:
-                  print(f"You did not input a valid member of {menu_category_name}")
+                  print(f"You did not input a valid member of {menu_selection_name}")
         else:
             # Tell the customer they didn't select a menu option
-            print(f"{menu_category} was not a menu option.")
+            print(f"{menu_selection} was not a menu option.")
     else:
         # Tell the customer they didn't select a number
         print("You didn't select a number.")
     # Ask the customer if they would like to order anything else
-    keep_ordering = input("Would you like to keep ordering? (Y)es or (N)o ").lower()
+    keep_ordering = input("Would you like to keep ordering? (Y)es or (N)o ")
     # 5. Check the customer's input
-    if(keep_ordering=="y"):
-    # Keep ordering
-        place_order = True
-    else:
-    # Exit the keep ordering question loop 
-        place_order = False
-
+    #converting the case of keep ordering before the check as indicated in the instructions but why 
+    match(keep_ordering):
+        case "y" | "Y":
+            keep_ordering.lower()
+            print(keep_ordering)
+            #continue to ask for more items
+            place_order= True
+        case "n" | "N":
+            keep_ordering.lower()
+            print(keep_ordering)
+            #exit the the ordering question loop
+            place_order=False
+        case _:
+            #instructions have asking again if like to keep ordering but do not specify what happens is something other than y or n is put in.  This allows a retry and then exits order loop if still not y or n.
+            retry_counter=0
+            while keep_ordering not in["y","Y","n","N"] and retry_counter<=1:
+                            retry_counter=retry_counter+1
+                            print("Please try again using Y for \"yes\" or N for \"No\"")
+                            keep_ordering = input("Would you like to keep ordering? (Y)es or (N)o ")
+                            place_order=True
+            match(keep_ordering):
+                case "y" | "Y":
+                    keep_ordering.lower()
+                    #continue to ask for more items
+                    place_order= True
+                case "n" | "N":
+                    keep_ordering.lower()
+                    #exit the the ordering question loop
+                    place_order=False
+                case _:
+                    keep_ordering="n"
+                    #exit the the ordering question loop
+                    place_order=False
 # Complete the order
 # Since the customer decided to stop ordering, thank them for
 # their order
@@ -186,17 +213,21 @@ print("--------------------------|--------|----------|-----------")
 # 6. Loop through the items in the customer's order
 # 7. Store the dictionary items as variables
 for y in range(len(order_list)):
-     # 8. Calculate the number of spaces for formatted printing
-    num_name_spaces=26-len(order_list[y]["Item name"])
-    num_price_spaces=6-len(str(order_list[y]["Price"]))
-    num_quantity_spaces=9-len(str(order_list[y]["Quantity"]))
+    #create item_name, price, and quantity variables from order_list
+    item_name=order_list[y]["Item name"]
+    price=order_list[y]["Price"]
+    quantity=order_list[y]["Quantity"]
+    # 8. Calculate the number of spaces for formatted printing
+    num_name_spaces=26-len(item_name)
+    num_price_spaces=6-len(str(price))
+    num_quantity_spaces=9-len(str(quantity))
     # 9. Create space strings
     spaces_name=" " * num_name_spaces
     spaces_price=" " * num_price_spaces
     spaces_quantity=" " * num_quantity_spaces
     # 10. Print the item name, price, and quantity, and item cost
-    print(f"{order_list[y]["Item name"]}{spaces_name}| ${order_list[y]["Price"]}{spaces_price}| {order_list[y]["Quantity"]}{spaces_quantity}| ${item_cost_lc[y]}")
+    print(f"{item_name}{spaces_name}| ${price}{spaces_price}| {quantity}{spaces_quantity}| ${item_cost_lc[y]}")
 #print out total for whole order
-# and print the prices.
+# and print the prices.  Sum done below, list comprehension on line 187 so could add onto receipt.
 print(f"\n\nOrder Total                                     ${sum(item_cost_lc)}\n\n\nWe strive for excellent service at variety food truck.\nPlease fill out our survey and receive a coupon for a\nfree drink on your next order by:\nGoing to varietyfoodtruck.com/survey and entering your\n     order number at the top of this receipt\nOR scanning the QR code on this receipt\nOR scanning the QR code on the front of the truck by the\n    menu and entering your order number at the top this\n    receipt.\n\nThank you and have a great day!")
    
